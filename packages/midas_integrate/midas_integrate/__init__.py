4cebd472e7d71b0549fe341988b3c043e28c8d46
Hemant Sharma <hemant@sharmas.biz>
2026-05-01 14:59:42 -0500

    midas-integrate: bump version to 0.2.0

--- packages/midas_integrate/midas_integrate/__init__.py
+++ packages/midas_integrate/midas_integrate/__init__.py
@@ -5,7 +5,7 @@ Public API surface (re-exports from submodules so users can do
 """
 from __future__ import annotations
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from midas_integrate.bin_io import (
     PXLIST_DTYPE,
